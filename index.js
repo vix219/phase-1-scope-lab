7baa5d087e682f96d5ef35b997279c55e1fc9633
VictoriaCarvalho <vix219@icloud.com>
2025-02-14 09:44:11 -0600

    add lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+
+var customerName = "bob";
+
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+    return customerName;
+}
+
+
+
+
+function setBestCustomer() {
+   bestCustomer = 'not bob';
+   return setBestCustomer;
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+    return overwriteBestCustomer;
+}
+
+const leastFavoriteCustomer = 'chad'
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'still chad'
+}
